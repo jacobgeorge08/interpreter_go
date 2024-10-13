0f6662438ffaf05b1749ffd9ac651d48383c763c
T460 <jacobgeorge08@gmail.com>
2024-10-13 23:42:38 +0530

    wrote a test and sample code for lexer

---
+++ token/token.go
@@ -0,0 +1,30 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LET      = "LET"
+	FUNCTION = "FUNCTION"
+)
